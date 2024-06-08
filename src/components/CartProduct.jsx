fb6258b1967fb14d021e0e6c3565028bef98016d
pratham meena <yashmeena274@gmail.com>
2024-06-08 19:52:19 +0530

    added quantity feature

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CartProduct = ({
   productImage,
@@ -6,6 +6,13 @@ const CartProduct = ({
   productDescription,
   productPrice,
 }) => {
+
+
+  const [quantity, setquantity] = useState(1)
+
+  console.log(quantity)
+
+
   return (
     <div className="cartProduct flex w-full justify-between items-center border-b border-zinc-800 py-3 px-5">
       <div className="cartProductLeft flex items-center gap-4">
@@ -30,11 +37,11 @@ const CartProduct = ({
       </div>
       <div className="cartProductRight flex items-center gap-10">
         <div className="quantity flex gap-4 items-center">
-          <div onClick={decreaseQuantityHandler} className="w-[2vw] h-[2vw] bg-zinc-800 rounded-full flex items-center justify-center">
+          <div onClick={()=> quantity > 1 && setquantity(quantity-1)} className="w-[2vw] h-[2vw] bg-zinc-800 rounded-full flex items-center justify-center">
             <i className="ri-subtract-line"></i>
           </div>
-          <div>1</div>
-          <div className="w-[2vw] h-[2vw] bg-zinc-800 rounded-full flex items-center justify-center">
+          <div>{quantity}</div>
+          <div onClick={()=> setquantity(quantity+1)} className="w-[2vw] h-[2vw] bg-zinc-800 rounded-full flex items-center justify-center">
             <i className="ri-add-line"></i>
           </div>
         </div>
